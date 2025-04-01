61a02c270dfbda857e818fa854032fc77751e4ab
Vitor Oliveira <vitorloliveira96@gmail.com>
2025-04-01 11:23:16 -0300

    atualização exercício05, aluno reprovado

--- exercicio05.py
+++ exercicio05.py
@@ -5,6 +5,11 @@ n3 = float(input("Digite a nota 3: "))
 media = (n1+n2+n3)/3
 
 if media >= 7:
-    print(f"O aluno foi aprovado {media}!")
+    print(f"O aluno foi aprovado com {media:.2f}!")
 else:
-    print(f"O aluno foi reprovado com média {media}!")
+    if media < 4:
+        print(f"O aluno foi reprovado com média {media:.2f}!")
+    else:
+        print(f"O aluno está de recuperação com média {media:.2f}!")
+
+
