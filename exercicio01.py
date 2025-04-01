e5faea366f62b0431edda14acafdb8db65176c2a
Vitor Oliveira <vitorloliveira96@gmail.com>
2025-04-01 08:50:28 -0300

    criação do exercicio01, adição de entradas e saídas (nome, idade, salário)

---
+++ exercicio01.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = int (input("Digite sua idade: "))
+salario = float (input("Digite seu salário: "))
+
+print (f"Seu nome é {nome}, você tem {idade} anos e seu salário é de R${salario}")
