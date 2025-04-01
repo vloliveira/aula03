489230f6115fdda1de8357113d0ff8f2d2fc962b
Vitor Oliveira <vitorloliveira96@gmail.com>
2025-04-01 09:40:36 -0300

    criação do exercício02, função das 4 operações matemáticas

---
+++ exercicio02.py
@@ -0,0 +1,12 @@
+num1 = int (input("Digite o primeiro número: "))
+num2 = int (input("Digite o segundo número: "))
+
+soma = num1+num2
+subtracao = num1-num2
+divisao = num1/num2
+multi =num1*num2
+
+print (f"A soma entre os números é de: {soma} \n"
+       f"A subtração entre os números é de: {subtracao}\n"
+       f"A divisão entre os números é de:{divisao}\n"
+       f"A multiplicação entre os números é de: {multi}")
