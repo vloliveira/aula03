831eaac9f52e42cede58b4b4f344b0d9e92c9ba1
Vitor Oliveira <vitorloliveira96@gmail.com>
2025-04-08 09:30:45 -0300

    Exercício 09, atividade dos meses

---
+++ exercício09.py
@@ -0,0 +1,28 @@
+num = int(input("Digite um número: "))
+
+if num < 1 or num > 12:
+    print("Digite um número válido")
+elif num == 1:
+    print("Janeiro")
+elif num == 2:
+    print("Fevereiro")
+elif num == 3:
+    print("Março")
+elif num == 4:
+    print("Abril")
+elif num == 5:
+    print("Maio")
+elif num == 6:
+    print("Junho")
+elif num == 7:
+    print("Julho")
+elif num == 8:
+    print("Agosto")
+elif num == 9:
+    print("Setembro")
+elif num == 10:
+    print("Outubro")
+elif num == 11:
+    print("Novembro")
+elif num == 12:
+    print("Dezembro")
